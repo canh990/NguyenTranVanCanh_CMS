7c05bd2b3eeae6cc39c95d8bae539fcf2a0a2981
canh990 <nguyencanhsstran@gmail.com>
2026-09-18 00:38:09 +0700

    commit module2

--- wordpress/wp-content/themes/twentytwentyone/archive.php
+++ wordpress/wp-content/themes/twentytwentyone/archive.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * The template for displaying archive pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * Module 2: Content (FIT-TDC Style Post Listing)
+ * Reference: http://fit.tdc.edu.vn
  *
  * @package WordPress
  * @subpackage Twenty_Twenty_One
@@ -23,12 +23,31 @@ $description = get_the_archive_description();
 		<?php endif; ?>
 	</header><!-- .page-header -->
 
+	<div class="fit-tdc-posts-container" style="max-width:680px; margin:25px auto; padding:0 15px;">
 	<?php while ( have_posts() ) : ?>
 		<?php the_post(); ?>
-		<?php get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) ); ?>
+		<!-- ═══ MODULE 2: FIT-TDC POST CARD ═══ -->
+		<article id="post-<?php the_ID(); ?>" <?php post_class( 'fit-tdc-post-card' ); ?>>
+			<!-- Date Box (Left) -->
+			<div class="fit-tdc-date-box">
+				<span class="fit-tdc-day"><?php echo esc_html( get_the_date( 'd' ) ); ?></span>
+				<span class="fit-tdc-month"><?php echo esc_html( 'THÁNG ' . get_the_date( 'm' ) ); ?></span>
+			</div>
+
+			<!-- Content Box (Right) -->
+			<div class="fit-tdc-content-box">
+				<h2 class="fit-tdc-post-title">
+					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+				</h2>
+				<div class="fit-tdc-post-excerpt">
+					<?php echo esc_html( function_exists( 'fit_tdc_get_post_excerpt' ) ? fit_tdc_get_post_excerpt( get_the_ID() ) : get_the_excerpt() ); ?>
+				</div>
+			</div>
+		</article><!-- #post-<?php the_ID(); ?> -->
 	<?php endwhile; ?>
 
 	<?php twenty_twenty_one_the_posts_navigation(); ?>
+	</div><!-- /.fit-tdc-posts-container -->
 
 <?php else : ?>
 	<?php get_template_part( 'template-parts/content/content-none' ); ?>
